87386768710bc5b5583e90246ffd6b1d862a9b7e
Jeek <garry@oneill.so>
2021-02-28 17:14:18 +1000

    delete

--- Private/Test-IsAdmin.ps1
+++
@@ -1,4 +0,0 @@
-function Test-IsAdmin {
-    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-    $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-}
