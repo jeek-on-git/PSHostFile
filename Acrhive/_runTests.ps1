76061476ea6dea00b0f6e9645bde360b53e53b7a
Jeek <42949686+garryoneill@users.noreply.github.com>
2021-12-05 17:15:43 +1000

    Delete Archive Directory
    Not for public consumption

--- Acrhive/_runTests.ps1
+++
@@ -1,8 +0,0 @@
-$file = "$PSScriptRoot"
-
-
-$container = New-PesterContainer -Path "$PSScriptRoot\*.Tests.ps1" -Data @{ folder = $folders }
-Invoke-Pester -Container $container -Output Detailed
-
-Invoke-Pester -Path 'd:\Garry\Repos\PSModules\PSHostFile\Tests'
-Invoke-Pester -Path 'd:\Garry\Repos\PSModules\PSHostFile\Tests' -Output 'Detailed'
