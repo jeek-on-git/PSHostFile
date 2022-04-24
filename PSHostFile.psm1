3d69a2678d92e18eed7509d573d160307dbaf641
Garry ONeill <goneill@ermpower.com.au>
2022-04-24 21:57:18 +1000

    fix formatting

--- PSHostFile.psm1
+++ PSHostFile.psm1
@@ -1,23 +1,14 @@
-$Public  = @(Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue)
-$Private = @(Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue)
+$public  = @(Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue)
+$private = @(Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue)
 
-Foreach($import in @($Public + $Private)) {
-
+foreach($import in @($public + $private)) {
     Try {
-
         . $import.FullName
-
     }
-
     Catch {
-
         Write-Error -Message "Failed to import function $($import.FullName): $_"
-
     }
 
 }
 
-Export-ModuleMember -Function $Public.Basename -Alias * #-Variable hostFile,hostFileObject
-
-# Set-Variable -Name HostFilePath -Value $null -Scope Script -Force
-# Set-Variable -Name HostFileObject -Value $null -Scope Script -Force
+Export-ModuleMember -Function $public.Basename -Alias *
